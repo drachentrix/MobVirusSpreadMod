d1d2ec3aceebbfd658c59d3a84e5220fbb9f0bb9
Dragonias <florian.schwanzer@htlstp.at>
2024-02-24 22:02:45 +0100

    Updatet todo list Added future idead list

--- src/main/java/org/drachentrix/plugins/mobvirusspread/block/custom/VoidBerryCropBlock.java
+++ src/main/java/org/drachentrix/plugins/mobvirusspread/block/custom/VoidBerryCropBlock.java
@@ -1,8 +1,6 @@
 package org.drachentrix.plugins.mobvirusspread.block.custom;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 
@@ -11,8 +9,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CarrotBlock;
-import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.EnderChestBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
